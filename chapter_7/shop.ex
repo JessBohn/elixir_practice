c5bc40825a86f427fbb580ade4d7d92ebc46e7a5
Jessica Bohn <jbohn.developer@gmail.com>
2019-03-12 16:11:02 -0500

    replace IO prompts with in checkout w/ ask_number function

--- chapter_7/shop.ex
+++ chapter_7/shop.ex
@@ -1,16 +1,8 @@
 defmodule Shop do
    def checkout() do
-      case ask_number("Quantity?") do
-         :error ->
-            IO.puts("It's not a number")
-         {quantity, _} ->
-            case ask_number("Price?") do
-               :error ->
-                  IO.puts("It's not a number")
-               {price, _} ->
-                  quantity * price
-            end
-      end
+      quantity = ask_number("Quantity?")
+      price = ask_number("Price?")
+      calculate(quantity, price)
    end
 
    def ask_number(message) do
