cf9977bf255db897ecb6bf5ad6c9a955d5b20264
Jessica Bohn <jbohn.developer@gmail.com>
2019-03-12 16:14:04 -0500

    add calculate function, handles errors for qty & price, when no errors does calculation

--- chapter_7/shop.ex
+++ chapter_7/shop.ex
@@ -5,6 +5,10 @@ defmodule Shop do
       calculate(quantity, price)
    end
 
+   def calculate(:error, _), do: IO.puts("Quantity is not a number")
+   def calculate(_, :error), do: IO.puts("Price is not a number")
+   def calculate({quantity, _}, {price, _}), do: quantity * price
+
    def ask_number(message) do
       message <> "\n"
          |> IO.gets
