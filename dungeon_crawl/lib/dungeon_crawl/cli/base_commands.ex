b1ded49bda81000e1a469abd66d89e2d764fef75
Jessica Bohn <jbohn.developer@gmail.com>
2019-03-13 11:27:04 -0500

    using error monads to handle failures

--- dungeon_crawl/lib/dungeon_crawl/cli/base_commands.ex
+++ dungeon_crawl/lib/dungeon_crawl/cli/base_commands.ex
@@ -1,7 +1,16 @@
-defmodule DungeonCrawl.CLI.BaseCommands do
-   alias Mix.Shell.IO, as: Shell
+# ADVANTAGES OF USING MONAD METHOD
+   # clear happy path of the function pipeline
+   # the error handling has been put at a unique point
+   # fxn always return a value, returning a consistent data structure that flags an error or a success
+# DISADVANTAGES
+   # Elixir doesn't have built-in support for monads, so you have to choose a monad library from community
+   # monad libraries may look disconnected from the rest of the language (~>> not as clean as |>)
 
-   @invalid_option {:error, "Invalid option"}
+defmodule DungeonCrawl.CLI.BaseCommands do
+   use Monad.Operators
+
+   alias Mix.Shell.IO, as: Shell
+   import Monad.Result, only: [success: 1, success?: 1, error: 1, return: 1]
 
    def display_options(options) do
       options
@@ -9,7 +18,8 @@ defmodule DungeonCrawl.CLI.BaseCommands do
       |> Enum.each(fn {option, index} ->
          Shell.info("#{index} - #{option}")
       end)
-      options
+
+      return(options)
    end
 
    def generate_question(options) do
@@ -17,17 +27,25 @@ defmodule DungeonCrawl.CLI.BaseCommands do
       "Which one? [#{options}]\n"
    end
 
+   # the integer parsing can result in an error
+   # check it using case with pattern matching
+   # when result is an error - use error/1 to return an error result with a message
+   # when parsed result is a valid number - use success/1 to return a success result wrapping the number
    def parse_answer(answer) do
       case Integer.parse(answer) do
-         :error ->
-            throw @invalid_option
-         {option, _} ->
-            option - 1
+         :error -> error("Invalid option")
+         {option, _} -> success(option - 1)
       end
    end
 
+   # same logic as parse_answer above
+   # when matching nil, return an error result
+   # when matching a number, return a success result
    def find_option_by_index(index, options) do
-      Enum.at(options, index) || throw @invalid_option
+      case Enum.at(options, index) do
+         nil -> error("Invalid option")
+         chosen_option -> success(chosen_option)
+      end
    end
 
    def ask_for_index(options) do
@@ -54,19 +72,23 @@ defmodule DungeonCrawl.CLI.BaseCommands do
       Shell.cmd("clear")
    end
 
+   # updated to reflect new Error Monad method
+
    def ask_for_option(options) do
-      # 'try do' can be omitted, but it makes the happy path harder to see
-      try do
-         options
-         |> display_options
-         |> generate_question
-         |> Shell.prompt
-         |> parse_answer
-         |> find_option_by_index(options)
-      catch # if the sequence above fails, the program will continue with below
-         {:error, message} ->
-            display_error(message)
-            ask_for_option(options)
+      # inside result, instead of the pipeline operator (|>), the bind operator (~>>) must be used to sequence the functions
+      result =
+         return(options)
+         # |> display_options - standard pipeline syntax for the line below
+         ~>> (&display_options/1) # must follow syntax - (bind operator) ~>> (&function/arity)
+         ~>> (&generate_question/1)
+         ~>> (&Shell.prompt/1)
+         ~>> (&parse_answer/1)
+         ~>> (&(find_option_by_index(&1, options)))
+      if success?(result) do
+         result.value
+      else
+         display_error(result.error)
+         ask_for_option(options)
       end
    end
 
